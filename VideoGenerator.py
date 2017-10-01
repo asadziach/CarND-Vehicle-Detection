435e822fa91f9b9890cff1d521304d05ef217f11
Asad Zia <asadzia@gmail.com>
2017-10-01 20:26:41 +0500

    YOLO tiny working on video

---
+++ VideoGenerator.py
@@ -0,0 +1,58 @@
+'''
+Created on Sep 23, 2017
+
+@author: asad
+'''
+from moviepy.editor import VideoFileClip
+from darkflow.net.build import TFNet
+import cv2
+
+class VideoLaneProcessor(object):
+    
+    '''
+    class attributes
+    '''
+
+    def __init__(self, camera_cal_pickle):
+        '''
+        Constructor
+        '''
+        options = {"model": "cfg/tiny-yolo-voc.cfg", "load": "bin/tiny-yolo-voc.weights", "threshold": 0.1, "gpu": 1.0, "batch": 32}
+        
+        self.tfnet = TFNet(options)
+        self.frame_count = 0   
+     
+
+    # Video processing pipeline
+    def process_image(self, image):
+    
+        result = self.tfnet.return_predict(image)
+        
+        #print(result)
+        
+        for box in result:
+            x1 = box['topleft']['x']
+            y1 = box['topleft']['y']
+            x2 = box['bottomright']['x']
+            y2 = box['bottomright']['y']
+            cv2.rectangle(image,(x1,y1),(x2,y2),(0,255,0),3)
+            label = str(box['label']) + " " + str(int(box['confidence']*100))
+            cv2.putText(
+                image, label, (x1, y1 - 12),
+                cv2.FONT_HERSHEY_SIMPLEX, 1e-3 * 600, (0,255,0),
+                1)              
+    
+        return image  
+        
+def main():
+    videoname = 'project_video'
+    output = videoname + '_output.mp4'
+    input  = videoname + '.mp4'
+    
+    clip = VideoFileClip(input)
+    processor = VideoLaneProcessor("camera_cal/wide_dist_pickle.p")
+    video_clip = clip.fl_image(processor.process_image)
+    video_clip.write_videofile(output, audio=False)
+    
+if __name__ == '__main__':
+    main()
